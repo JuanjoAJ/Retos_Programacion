b0776993e5dab01849e23a1457c282bf2ee699d6
Juan José <148000889+JuanjoAJ@users.noreply.github.com>
2023-12-18 14:26:09 +0100

    reto4

--- Reto2022_003Primo.java
+++ Reto2022_003Primo.java
@@ -8,6 +8,29 @@
  * Enunciado: Escribe un programa que se encargue de comprobar si un número es o no primo.
  * Hecho esto, imprime los números primos entre 1 y 100.
  */
- public class Reto2022_003Primo {
+public class Reto2022_003Primo {
+
+ public static void main(String[] args) {
+  for (int i = 0; i <= 100; i++) {
+   if (Primo(i)) System.out.println(i);
+  }
+ }
+
+ public static boolean Primo(int numero) {
+  if (numero == 2 || numero == 3 || numero == 5 || numero == 7) return true;
+  if (numero == 1 || numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 || numero % 7 == 0) return false;
+  return true;
+ }
+
+ /*Otra manera de hacerlo sería:
+  public static boolean Primo (int numero){
+   if (numero<=1){
+   return false;}
+   for (int i=2; i<numero; i++){
+   if (numero%i==0){
+   return false;}
+   }
+   return true;
+   }*/
 
 }
--- Reto2022_004Poligono.java
+++ Reto2022_004Poligono.java
@@ -12,6 +12,39 @@
  */
 
 
-
+import java.util.Scanner;
 public class Reto2022_004Poligono {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Bienvenido al programa de cálculo del area. Elija las siguientes opciones:" +
+                "\n 1. Triángulo. \n 2. Cuadrado. \n 3. Rectángulo");
+        int eleccion= scanner.nextInt();
+
+        switch (eleccion){
+            case 1:
+                System.out.println("Introduzca la base");
+                int base= scanner.nextInt();
+                System.out.println("Introduzca la altura");
+                int altura=scanner.nextInt();
+                System.out.println("El area del triangulo es " +((base*altura)/2));
+              break;
+
+            case 2:
+                System.out.println("Introduzca el lado del cuadrado");
+                int lado= scanner.nextInt();
+                System.out.println("El area del cuadrado es " + (int)Math.pow(lado,2));
+                break;
+            case 3:
+                System.out.println("Introduzca el largo del rectángulo");
+                lado=scanner.nextInt();
+                System.out.println("Introduzca la altura del rectángulo");
+                altura= scanner.nextInt();
+                System.out.println("El area del rectángulo es " + lado*altura);
+                break;
+
+            default:
+                System.out.println("Lo siento, aún no podemos calcular eso");
+                break;
+        }
+    }
 }
