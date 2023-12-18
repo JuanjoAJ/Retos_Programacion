e5a121141535165babf7d222d7f98a0eed435276
Juan José <148000889+JuanjoAJ@users.noreply.github.com>
2023-12-18 21:14:04 +0100

    update

--- Reto2022/Reto2022_009Morse.java
+++ Reto2022/Reto2022_009Morse.java
@@ -11,5 +11,15 @@ package Reto2022;
  * - En morse se soporta raya "—", punto ".", un espacio " " entre letras o símbolos y dos espacios entre palabras "  ".
  * - El alfabeto morse soportado será el mostrado en https://es.wikipedia.org/wiki/Código_morse.
  */
+
+import java.util.*;
+
 public class Reto2022_009Morse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el texto a traducir en morse: ");
+        String frase = scanner.nextLine().toLowerCase();
+        String morse = frase.replaceAll("a", ".- ").replaceAll("b", "-... ").replaceAll("c", "-.-. ").replaceAll("d", "-.. ").replaceAll("e", ". ").replaceAll("f", "..-. ").replaceAll("g", "--. ").replaceAll("h", ".... ").replaceAll("i", ".. ").replaceAll("j", ".--- ").replaceAll("k", "-.- ").replaceAll("l", ".-.. ").replaceAll("m", "-- ").replaceAll("n", "-. ").replaceAll("o", "--- ").replaceAll("p", ".--. ").replaceAll("q", "--.- ").replaceAll("r", ".-. ").replaceAll("s", "... ").replaceAll("t", "- ").replaceAll("u", "..-").replaceAll("v", "...-").replaceAll("w", ".-- ").replaceAll("x", "-..- ").replaceAll("y", "-.-- ").replaceAll("z", "--.. ").replaceAll("1", ".---- ").replaceAll("2", "..--- ").replaceAll("3", "...-- ").replaceAll("4", "....-").replaceAll("5", "..... ").replaceAll("6", "-.... ").replaceAll("7", "--... ").replaceAll("8", "---.. ").replaceAll("9", "----. ").replaceAll("0", "-----");
+        System.out.println(frase + "\n" + morse);
+    }
 }
