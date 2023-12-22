403eb971ff402ae46c6a1c8cdb9bc8b59a659d6b
Juan José <148000889+JuanjoAJ@users.noreply.github.com>
2023-12-22 21:42:18 +0100

    up

--- Reto2022/Reto2022_015Dias.java
+++ Reto2022/Reto2022_015Dias.java
@@ -14,22 +14,69 @@ package Reto2022;
  */
 
 import java.util.Scanner;
-import java.util.Date;
+
+
 public class Reto2022_015Dias {
-    public static void main(String[] args){
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Por favor introduzca una fecha :");
-        String fecha= scanner.next();
-        System.out.println("Introduzca una segunda fecha");
-        String fechaDos= scanner.next();
 
 
-        //TODO
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor introduzca una fecha (dd/MM/yyyy) :");
+        String fechaUno = scanner.next();
+        System.out.println("Introduzca una segunda fecha (dd/MM/yyyy)");
+        String fechaDos = scanner.next();
 
 
+
+        String[] a = fechaUno.split("/");
+        String[] b = fechaDos.split("/");
+
+        int diaUno= Integer.parseInt(a[0]);
+        int mesUno = Integer.parseInt(a[1]);
+        int anioUno = Integer.parseInt(a[2]);
+        mesUno = mes(mesUno, anioUno);
+        anioUno = anio(anioUno);
+
+
+        int diaDos = Integer.parseInt(b[0]);
+        int mesDos = Integer.parseInt(b[1]);
+        int anioDos = Integer.parseInt(b[2]);
+        mesDos = mes(mesDos, anioDos);
+        anioDos = anio(anioDos);
+
+
+        int dias = Math.abs((anioDos - anioUno) + (mesDos - mesUno) + (diaDos - diaUno));
+
+        System.out.printf("Entre %s y %s existe una diferencia de %d días.", fechaUno, fechaDos, dias);
 
     }
 
+    public static int anio(int anio) {
+        if (anio%4 == 0 && anio%100!= 0 || anio%400==0 ){
+            anio*=366;
+            return anio;
+        }else {
+            anio*=365;
+            return anio;
+        }
+    }
 
-
+    public static int mes (int mes, int anio){
+        if (mes==2){
+            if(anio%4 == 0 && anio%100!= 0 || anio%400==0 ){
+                mes*=29;
+                return mes;
+            }
+            mes*=28;
+            return mes;
+        }
+        else if (mes==4 || mes==6 || mes==9 || mes==11 ){
+            mes*=30;
+            return mes;
+        }else{
+            mes*=31;
+            return mes;
+        }
+    }
 }
---
+++ Reto2022/Reto2022_19ConversorMiliseg.java
@@ -0,0 +1,12 @@
+package Reto2022;
+/*
+ * Reto #19
+ * CONVERSOR TIEMPO
+ * Fecha publicación enunciado: 09/05/22
+ * Fecha publicación resolución: 16/05/22
+ * Dificultad: FACIL
+ *
+ * Enunciado: Crea una función que reciba días, horas, minutos y segundos (como enteros) y retorne su resultado en milisegundos.
+ */
+public class Reto2022_19ConversorMiliseg {
+}
